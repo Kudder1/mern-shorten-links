b81da3cd84a193c9eb1dffa3755ebc8594443044
Vladimir Fishelev <Vladimir.Fishelev@dataart.com>
2021-03-12 18:17:33 +0200

    Initial commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export default function Navbar() {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+
+    const logoutHandler = event => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+    return (
+        <nav>
+            <div className="nav-wrapper blue darken-1" style={{padding: '0 2rem'}}>
+                <span className="brand-logo">Shortening Links</span>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <NavLink to="/create">Create</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/links">Links</NavLink>
+                    </li>
+                    <li>
+                        <a onClick={logoutHandler} href="/">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
